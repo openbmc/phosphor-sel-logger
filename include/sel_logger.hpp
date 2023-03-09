31d9032f598a5d416461b8c607a61e2f8c2cca38
BonnieLo-wiwynn <Bonnie_Lo@wiwynn.com>
2023-03-09 17:17:07 +0800

    Support redfish dbus based logging for pulse event monitor
    Send log entries with Redfish MessageId and MessageArgs via logging
    srvice. Redfish will get entrise on dbus.
    
    Expected Redfish event entry:
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/59",
      "@odata.type": "#LogEntry.v1_9_0.LogEntry",
      "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/59/attachment",
      "Created": "2018-03-14T11:22:50.117+00:00",
      "EntryType": "Event",
      "Id": "59",
      "Message": "Host system DC power is on",
      "MessageId": "OpenBMC.0.1.DCPowerOn",
      "Modified": "2018-03-14T11:22:50.117+00:00",
      "Name": "System Event Log Entry",
      "Resolved": false,
      "Severity": "OK"
    }
    
    Tested: Tested and verified.
    Change-Id: I103546e9104c0031d95ee3fbd7574230e47c19e3
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- include/sel_logger.hpp
+++ include/sel_logger.hpp
@@ -42,3 +42,6 @@ static uint16_t
     selAddSystemRecord(const std::string& message, const std::string& path,
                        const std::vector<uint8_t>& selData, const bool& assert,
                        const uint16_t& genId, T&&... metadata);
+
+static void selAddWithMethodCall(std::string message, std::string severity,
+                                 std::map<std::string, std::string> additionalData);
